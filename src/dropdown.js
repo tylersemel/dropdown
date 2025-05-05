b3add41e3ab65cb27aa1a4164876bb4d24117a07
tylersemel <tylersemel@gmail.com>
2025-05-05 11:51:08 -0400

    Set up files for npm package

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,14 +1,6 @@
-exports.dropdown = (() => {
-    const dropdowns = document.querySelectorAll('.dropdown');
-
-    for (const dropdown of dropdowns) {
-        dropdown.addEventListener('click', handleClickDropdown);
-    }
-
-    function handleClickDropdown (event) {   
+export function setDropdown(dropdown) {
+    dropdown.addEventListener('click', (event) => {
         const menu = event.target.parentNode.querySelector('.dropdown-list');
-
         menu.classList.toggle('visible');
-    }
-});
-
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { dropdown } from "./dropdown.js";
+import { setDropdown } from "./dropdown.js";
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
@@ -7,10 +7,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 console.log("I'm working!");
 
+const dropdowns = document.querySelectorAll('.dropdown');
 
-
-
-dropdown();
+for (const dropdown of dropdowns) {
+    setDropdown(dropdown);
+}
 
 
 
